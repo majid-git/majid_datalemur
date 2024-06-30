5c239462cd054e9ff0e6c6ac5622864f3c1e2ccd
majid-git <majid.ahmed44@gmail.com>
2024-06-30 20:24:43 +0530

    1_candidates_problem

--- 1_candidates_problem.sql
+++ 1_candidates_problem.sql
@@ -5,8 +5,10 @@ select
 	where 
 		skill in ('Python','Tableau','PostgreSQL') 
 	group by 
-		candidate_id 
+		candidate_id -- groups candidate by candidate_id
 	having 
-		count(skill)>=3 
+		count(skill)>=3 -- here it will filter candidate having all the 3 skills
 	order by 
 		candidate_id;
+
+-- requirement is to fetch candidate having matching skills of Data Science (Python, Tableau,PostgreSQL)
